ff3576ac44fa76a94250a13f4d2693b5e03c2780
Stas <stas_sever@mail.ru>
2024-06-10 20:25:24 +0300

    added lesson counting field to serializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    count_lesson = SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_count_lesson(self, course):
+        return Lesson.objects.filter(course=course).count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
