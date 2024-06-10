793a13ff7068a6ddc1adbdb64cde5fdb5fa285d4
Stas <stas_sever@mail.ru>
2024-06-10 21:12:55 +0300

    added fixtures

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from users.models import User
+from users.models import User, Payments
 
 
 @admin.register(User)
@@ -7,3 +7,10 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'first_name', 'last_name', 'is_active',)
     list_filter = ('is_active',)
     search_fields = ('email',)
+
+
+@admin.register(Payments)
+class PaymentsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'payment_date', 'payment_amount', 'payment_method',)
+    list_filter = ('user', 'payment_method',)
+    search_fields = ('paid_course', 'paid_lesson',)
