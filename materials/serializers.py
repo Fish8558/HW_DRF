2937889e8a6de0ee1e7a4d1c7c5b68f7df61299c
Stas <stas_sever@mail.ru>
2024-06-10 21:18:05 +0300

    modified serializer, add lesson list

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
-
 from materials.models import Course, Lesson
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
 
 
 class CourseSerializer(serializers.ModelSerializer):
     count_lesson = SerializerMethodField()
+    lesson_list = LessonSerializer(source='lesson_set', many=True)
 
     class Meta:
         model = Course
@@ -13,9 +19,3 @@ class CourseSerializer(serializers.ModelSerializer):
 
     def get_count_lesson(self, course):
         return Lesson.objects.filter(course=course).count()
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
